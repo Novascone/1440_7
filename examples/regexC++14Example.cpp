0621b83b91ab855fb4bb41996090767c42b50529
Erik on Voyager2 <ewfalor@gmail.com>
2017-12-01 16:40:47 -0700

    examples/regexC++14Example.cpp has URL-matching regex
    This commit contains the regular expression we wrote together in class
    today. It should be sufficient for our purposes on this assignment, so
    you don't *have* to improve it - I'd rather you spend the time working
    on recursion and using the std::map ADT and the cURLpp libraries.

--- examples/regexC++14Example.cpp
+++ examples/regexC++14Example.cpp
@@ -13,11 +13,17 @@
 #include <regex>
 
 int main () {
+    // TODO: put some HTML <a> tags in here for testing with the hrefRegexSource regex
     std::string haystack("this subject has a submarine as a subsequence\n"
             "subbing. submitting\n"
             "submission, substitute\n"
             "semper ubi? sub! ubi subversion"
             );
+
+    // Here is the regex we built in class. It may not be completely correct, but it should be
+    // good enough for your assignment. Do feel free to improve it, if you wish :D
+    //
+    std::string hrefRegexSource("<[ \n\t]*a[ \n\t]+.*href=\"(https*://)*([^/]+)*\/*([^\"]+)\"");
 
     std::string regexSourceCode("\\b(sub)([^ \n\t]*)");   // matches words beginning by "sub"
     std::regex needle(regexSourceCode);
